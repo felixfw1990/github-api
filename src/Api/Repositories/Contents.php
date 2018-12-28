cdc8903e77e5f1087248215985bedbc86f5587c6
Felix <felixfw1111@gmail.com>
2018-12-28 16:01:21 +0800

    #

--- src/Api/Repositories/Contents.php
+++ src/Api/Repositories/Contents.php
@@ -56,22 +56,6 @@ class Contents
         ->setQuery($queue)
         ->get(API::REPOSITORIES['CRContents']);
 
-//        $errors = $result['errors'] ?? [];
-//        $code = Helper::generalArray($errors, 'code');
-//
-//        if (in_array('too_large', $code))
-//        {
-//            //@TODO 最终考虑结果，后端不做下载，有个app data tree 可以到达100m
-//            //如果是图片， 直接渲染，
-//            //如果是文件，直接下载。
-//            //如果是可显示的大文件，先下载，再渲染
-//            //如果是小文件，直接读取数据封装下载或者直接显示。
-//            //文件太大，超过 1M
-//            //调用Git Data API 请求小于100M的
-//        }
-//
-//        Helper::p($result);
-
         return $result;
     }
 
